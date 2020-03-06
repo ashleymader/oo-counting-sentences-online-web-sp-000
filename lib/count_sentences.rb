f87f3cab1e5d4cf611a9c20e3d7fdafb5712e1cc
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-06 22:50:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,7 @@ class String
   end
 
   def count_sentences
-    self.delete_if(/(\.|\?|\!)/)
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
