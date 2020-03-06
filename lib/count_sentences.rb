8649a6bfeeffbeb20c6d1fc02ce308e1ee99734c
Ashley M Mader <ashley.mader12@gmail.com>
2020-03-06 22:42:45 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    result = self.split(/(\.|\?|\!)/).reject {|string| string.empty?}
+    result = self.split(/(\.|\?|\!)/)
     result.length
   end
 end
